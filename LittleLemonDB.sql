cbd36ddf1fdac86d1107d4a14692cd43a8670332
nduba stephen <nduba.stephen@yahoo.com>
2023-06-06 19:02:26 -0700

    capstone week 2 tasks

--- LittleLemonDB.sql
+++ LittleLemonDB.sql
@@ -1,134 +1,19 @@
--- MySQL Workbench Forward Engineering
+CREATE VIEW OrdersView AS SELECT OrderID, Quantity, TotalCost FROM Orders WHERE Quantity >2;
 
-SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
-SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
-SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
+SELECT Customers.CustomerID, CONCAT(Customers.FirstName, ",", Customers.LastName) AS "Full NAme", Orders.OrderID, Orders.TotalCost, MenuItem.CourseName, Menus.MenuName FROM Customers INNER JOIN Orders ON Customers.CustomerID = Orders.CustomerID INNER JOIN MenuItem ON Orders.OrderID = MenuItem.OrderID INNER JOIN Menus ON MenuItem.MenuItemID = Menus.MenuItemID ORDER BY Orders.TotalCost ASC;
 
--- -----------------------------------------------------
--- Schema LittleLemonDB
--- -----------------------------------------------------
+SELECT MenuName FROM Menus WHERE  (SELECT OrderID FROM Orders WHERE Quantity > 2);
 
--- -----------------------------------------------------
--- Schema LittleLemonDB
--- -----------------------------------------------------
-CREATE SCHEMA IF NOT EXISTS `LittleLemonDB` DEFAULT CHARACTER SET utf8 ;
-USE `LittleLemonDB` ;
+CREATE PROCEDURE GetMaxQuantity()
+SELECT Quantity FROM Orders Order BY Quantity DESC LIMIT 1;
 
--- -----------------------------------------------------
--- Table `LittleLemonDB`.`CustomerDetails`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `LittleLemonDB`.`CustomerDetails` (
-  `CustomerID` INT NOT NULL,
-  `FirstName` VARCHAR(45) NOT NULL,
-  `LastName` VARCHAR(45) NOT NULL,
-  `PhoneNo` INT NOT NULL,
-  `Email` VARCHAR(45) NOT NULL,
-  PRIMARY KEY (`CustomerID`))
-ENGINE = InnoDB;
+CALL GetMaxQuantity();
 
+PREPARE GetOrderDetail FROM 'SELECT OrderID, Quantity, TotalCost FROM Orders WHERE CustomerID = ?';
+SET @custormer_id = 1;
+EXECUTE GetOrderDetail USING @customer_id;
 
--- -----------------------------------------------------
--- Table `LittleLemonDB`.`Bookings`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `LittleLemonDB`.`Bookings` (
-  `BookingID` INT NOT NULL,
-  `BookingDate` DATE NOT NULL,
-  `TableNo` INT NOT NULL,
-  `CustomerID` INT NOT NULL,
-  PRIMARY KEY (`BookingID`),
-  INDEX `customer_id_key_idx` (`CustomerID` ASC) VISIBLE,
-  CONSTRAINT `customer_id_key`
-    FOREIGN KEY (`CustomerID`)
-    REFERENCES `LittleLemonDB`.`CustomerDetails` (`CustomerID`)
-    ON DELETE CASCADE
-    ON UPDATE CASCADE)
-ENGINE = InnoDB;
+CREATE PROCEDURE CancelOrder(order_id INT)
+DELETE FROM Orders WHERE OrderID = order_id;
+CALL CancelOrder(5);
 
-
--- -----------------------------------------------------
--- Table `LittleLemonDB`.`Orders`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `LittleLemonDB`.`Orders` (
-  `OrderID` INT NOT NULL,
-  `OrderDate` DATE NOT NULL,
-  `Quantity` INT NOT NULL,
-  `TotalCost` DECIMAL NOT NULL,
-  `CustomerID` INT NOT NULL,
-  PRIMARY KEY (`OrderID`),
-  INDEX `customer_id_key_idx` (`CustomerID` ASC) VISIBLE,
-  CONSTRAINT `customer_id`
-    FOREIGN KEY (`CustomerID`)
-    REFERENCES `LittleLemonDB`.`CustomerDetails` (`CustomerID`)
-    ON DELETE CASCADE
-    ON UPDATE CASCADE)
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
--- Table `LittleLemonDB`.`Menu`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `LittleLemonDB`.`Menu` (
-  `MenuID` INT NOT NULL,
-  `Cuisines` VARCHAR(45) NOT NULL,
-  `Starters` VARCHAR(45) NOT NULL,
-  `Courses` VARCHAR(45) NOT NULL,
-  `Drinks` VARCHAR(45) NOT NULL,
-  `Deserts` VARCHAR(45) NOT NULL,
-  `BookingID` INT NOT NULL,
-  PRIMARY KEY (`MenuID`),
-  INDEX `booking_id_key_idx` (`BookingID` ASC) VISIBLE,
-  CONSTRAINT `booking_id_key`
-    FOREIGN KEY (`BookingID`)
-    REFERENCES `LittleLemonDB`.`Bookings` (`BookingID`)
-    ON DELETE CASCADE
-    ON UPDATE CASCADE)
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
--- Table `LittleLemonDB`.`DeliveryStatus`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `LittleLemonDB`.`DeliveryStatus` (
-  `DeliveryID` INT NOT NULL,
-  `DeliveryDate` DATE NOT NULL,
-  `Status` VARCHAR(45) NOT NULL,
-  `OrderID` INT NOT NULL,
-  PRIMARY KEY (`DeliveryID`),
-  INDEX `order_id_key_idx` (`OrderID` ASC) VISIBLE,
-  CONSTRAINT `order_id_key`
-    FOREIGN KEY (`OrderID`)
-    REFERENCES `LittleLemonDB`.`Orders` (`OrderID`)
-    ON DELETE CASCADE
-    ON UPDATE CASCADE)
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
--- Table `LittleLemonDB`.`Staff`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `LittleLemonDB`.`Staff` (
-  `StaffID` INT NOT NULL,
-  `Role` VARCHAR(45) NOT NULL,
-  `Salary` DECIMAL NOT NULL,
-  `MenuID` INT NOT NULL,
-  `DeliveryID` INT NOT NULL,
-  `Staffcol` VARCHAR(45) NOT NULL,
-  PRIMARY KEY (`StaffID`),
-  INDEX `menu_id_key_idx` (`MenuID` ASC) VISIBLE,
-  INDEX `delivery_id_key_idx` (`DeliveryID` ASC) VISIBLE,
-  CONSTRAINT `menu_id_key`
-    FOREIGN KEY (`MenuID`)
-    REFERENCES `LittleLemonDB`.`Menu` (`MenuID`)
-    ON DELETE CASCADE
-    ON UPDATE CASCADE,
-  CONSTRAINT `delivery_id_key`
-    FOREIGN KEY (`DeliveryID`)
-    REFERENCES `LittleLemonDB`.`DeliveryStatus` (`DeliveryID`)
-    ON DELETE CASCADE
-    ON UPDATE CASCADE)
-ENGINE = InnoDB;
-
-
-SET SQL_MODE=@OLD_SQL_MODE;
-SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
-SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
